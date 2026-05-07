efd6631d424c0ad434c084a070219e298261345e
funkboyfresh <ryanplayle@gmail.com>
2026-05-07 01:56:53 -0500

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -702,8 +702,7 @@ function renderLevel4(container, footer) {
         terminal.appendChild(node);
     });
     
-    bridge.appendChild(terminal);
-    // --- [ END OF TERMINAL OVERHAUL ] ---
+  bridge.appendChild(terminal);
 
     const btnWrap = document.createElement('div'); 
     btnWrap.style.cssText = 'display:flex; gap:10px; margin-top: auto; padding: 20px 0;';
