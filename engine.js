20b729bd0d54e5da6ab8397957a5f82e756ed529
funkboyfresh <ryanplayle@gmail.com>
2026-05-08 18:19:51 -0500

    Update engine.js

--- engine.js
+++ engine.js
@@ -65,28 +65,6 @@ function addEnergy(baseAmount) {
     updateHUD();
 }
 
-function investOffering(deityKey, towerId) {
-    const currentLevel = state.pantheon[deityKey];
-    const isKeystoneNode = (currentLevel + 1) % 6 === 0;
-    const isMajorNode = currentLevel === 30;
-    
-    let cost = 1;
-    if (isMajorNode) cost = 50;
-    else if (isKeystoneNode) cost = 10;
-
-    if (state.offerings >= cost && currentLevel < 31) {
-        state.offerings -= cost;
-        state.pantheon[deityKey]++;
-        
-        triggerHaptic(50);
-        save();
-        
-        // Re-render the UI
-        renderAscensionTower(towerId);
-    } else {
-        showSoftWarning("THE VOID DEMANDS MORE OFFERINGS");
-    }
-}
 
 function save() { 
     localStorage.setItem('sectors', JSON.stringify(state.sectors));
